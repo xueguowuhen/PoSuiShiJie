f9ceb514c6bdc2a9baa072ada68f304af6302611
xueguowuhen <78350777+xueguowuhen@users.noreply.github.com>
2024-10-18 15:54:53 +0800

    战斗UI界面修改

---
+++ Assets/Scripts/UIWindow/UIWnd/StartGameWnd.cs
@@ -0,0 +1,25 @@
+/****************************************************
+    文件：StartGameWnd.cs
+    作者：无痕
+    邮箱: 1450411269@qq.com
+    日期：2024/10/14 21:35:43
+    功能：Nothing
+*****************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameWnd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
